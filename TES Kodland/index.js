aa6a1153536c97f94f3a74bc82b444cf1deefe89
Fr3ezy <115059063+Fr3ezy@users.noreply.github.com>
2022-11-14 21:18:08 +0700

    Add files via upload

---
+++ TES Kodland/index.js
@@ -0,0 +1,13 @@
+// button
+let toogleState = false;
+
+let toogleNav = function (){
+    let element = document.getElementById ('navbar-nav')
+    if(toogleState == false){
+        element.style.display= 'flex';
+        toogleState = true;
+    }else{
+        element.style.display= 'none';
+        toogleState = false;
+    }
+}
